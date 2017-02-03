fef2f2eff8c4f4cd9c08577e82f18beebe492a2f
luckygxf <403261934@qq.com>
2017-02-03 20:49:04 +0800

    start topPicture

--- index.php
+++ index.php
@@ -6,12 +6,13 @@
 		<link rel="stylesheet" href="firstMenu.css" type="text/css" />
 	-->
 	<link rel="stylesheet" href="firstMenu.css" type="text/css" />
+	<link rel="stylesheet" href="topPicture.css" type="text/css" />
 	<?php include 'menu.php';?>
 
-	<div>
-		<br>
-			顶部图片显示
-		<br>
+	<div class="topPicture">	
+		<div class="text">
+			旅游资源管理与定制
+		</div>
 	</div>
 	
 	<h1>欢迎访问我们的首页！</h1>
--- menu.php
+++ menu.php
@@ -3,14 +3,16 @@
 	echo '
 		<div class="firstMenu">
 			<div class="firstMenuLogoStyle">
-				<img src="" alt="logo"/>
+				
 			</div>
 			<div class="topMenuText">
 				<a href="/index.asp">首页</a> |
-				<a href="/html/index.asp">HTML 教程</a> |
-				<a href="/css/index.asp">CSS 教程</a> |
-				<a href="/js/index.asp">JavaScript 教程</a> |
-				<a href="/php/index.asp">PHP 教程</a>
+				<a href="/html/index.asp">资源</a> |
+				<a href="/css/index.asp">资源管理VS旅行定制</a> |
+				<a href="/js/index.asp">国际商务</a> |
+				<a href="/php/index.asp">媒体传播</a> | 
+				<a href="/php/index.asp">关于我们</a> | 
+				<a href="/php/index.asp">动态</a> | 
 			</div>
 		</div>
 	';
