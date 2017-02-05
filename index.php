653fc2e62223b9d005ac60416484915164e3c1b2
luckygxf <403261934@qq.com>
2017-02-05 22:43:01 +0800

    start dev toppics

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 	<head>
-		<title>Demo</title>
+		<title>南博卓越</title>
 		<link rel="shortcut icon" href="pictures/favicon.ico">
 		
 	</head>
@@ -10,20 +10,55 @@
 	-->
 	<link rel="stylesheet" href="css/firstMenu.css" type="text/css" />
 	<link rel="stylesheet" href="css/topPicture.css" type="text/css" />
+	<link href="css/pics.css" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="js/jquery.js"></script>
+	<!---->	
+	<script type="text/javascript" src="js/images.js"></script>
 	<?php include 'menu.php';?>
-
-	<div class="topPicture">	
-		<div class="text" id="mainPageText">
-			旅游资源管理与定制
+	<!--
+		<div class="topPicture">	
+			<div class="text" id="mainPageText">
+				旅游资源管理与定制
+			</div>
 		</div>
-		<div class="pictrueGallery">
-			<img src="pictures/picgalleryshow1.png"/>
-			<img src="pictures/picgalleryshow1.png"/>
-			<img src="pictures/picgalleryshow1.png"/>
+	-->
+	<div class="Container">
+		<div class="image" id="bigpics" title=""><img src="temp/four_find/s1f.jpg" /></div>
+		<!--
+		temp/four_find/s1f.jpg
+		-->
+		<div class="switch">
+			<div class="icon1"><a href="#" title="上一个" onfocus="this.blur();">上一个</a></div>
+			<div class="switch_center" id="pics">
+				<ul>
+					<li><a href="#" class="on"><img src="temp/four_find/s1.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+				</ul>
+		  </div>
+			<div class="icon2"><a href="#" title="下一个" onfocus="this.blur();">下一个</a></div>
+			<div class="clear"></div>
 		</div>
+		
+		
 	</div>	
 	<?php include 'foot.php'?>
-	<script type="text/javascript" src="mainPageAutoMove.js"></script>
+	<!---->
+	<!--<script type="text/javascript" src="mainPageAutoMove.js"></script>-->	
 </body>
 
 </html>
