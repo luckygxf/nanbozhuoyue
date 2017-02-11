824d06505e45d2437ce33d11f869fa2bcc7dce73
luckygxf <403261934@qq.com>
2017-02-12 00:34:25 +0800

    start dev second menu content

--- menu.php
+++ menu.php
@@ -1,46 +1,45 @@
 <link rel="stylesheet" href="css/topMenu.css" type="text/css" />
-<script src="jquery-1.7.1.min.js" type="text/javascript"></script>
+<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
 <script type="text/javascript" src="js/topMenu.js"></script>
-<?php
+<!--
 	//顶部目录菜单
-	echo '
-		<div class="firstMenu">
-			<div class="firstMenuLogoStyle">
-				
-			</div>
-			<div class="navigation-up">
-				<div class="navigation-inner">
-					<div class="navigation-v3">
-						<ul>
-							<li class="nav-up-selected-inpage" _t_nav="home">
-								<h2>
-									<a href="index.php">首页</a>
-								</h2>
-							</li>
-							<li class="" _t_nav="product">
-								<h2>
-									<a href="resourcesManage.php">资源管理VS旅行定制</a>
-								</h2>
-							</li>
-							<li class="" _t_nav="wechat">
-								<h2>
-									<a href="#">国际商务</a>
-								</h2>
-							</li>
-							<li class="" _t_nav="solution">
-								<h2>
-									<a href="#">关于我们</a>
-								</h2>
-							</li>
-							<li class="" _t_nav="cooperate">
-								<h2>
-									<a href="#">动态</a>
-								</h2>
-							</li>
-						</ul>
-					</div>
-				</div>
+-->
+<div class="firstMenu">
+	<div class="firstMenuLogoStyle">
+		
+	</div>
+	<div class="navigation-up">
+		<div class="navigation-inner">
+			<div class="navigation-v3">
+				<ul>
+					<li class="nav-up-selected-inpage" _t_nav="home">
+						<h2>
+							<a href="index.php">首页</a>
+						</h2>
+					</li>
+					<li class="" _t_nav="product">
+						<h2>
+							<a href="resourcesManage.php">资源管理VS旅行定制</a>
+						</h2>
+					</li>
+					<li class="" _t_nav="wechat">
+						<h2>
+							<a href="#">国际商务</a>
+						</h2>
+					</li>
+					<li class="" _t_nav="solution">
+						<h2>
+							<a href="#">关于我们</a>
+						</h2>
+					</li>
+					<li class="" _t_nav="cooperate">
+						<h2>
+							<a href="#">动态</a>
+						</h2>
+					</li>
+				</ul>
 			</div>
 		</div>
-	';
-?>
+	</div>
+</div>
+
