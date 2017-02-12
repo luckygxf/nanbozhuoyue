4daaca1df495d7d52b25766880c083f3b84ee71c
luckygxf <403261934@qq.com>
2017-02-12 13:12:19 +0800

    finish second menu ajax

--- resourcesManage.php
+++ resourcesManage.php
@@ -2,14 +2,59 @@
 	<head>
 		<title>资源管理与旅游定制</title>
 		<link rel="shortcut icon" href="pictures/favicon.ico">
+		<link rel="stylesheet" href="css/resourcesManage.css" type="text/css" />
+		<link rel="stylesheet" href="css/resourceManageTopPic.css" type="text/css" />
+		<link rel="stylesheet" href="css/main.css" type="text/css" />
+		<link rel="stylesheet" href="css/secondMenu.css" type="text/css" />
+		<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
+		<script src="js/secondMenu.js" type="text/javascript"></script>
+		<script type="text/javascript">
+			//second menu add ajax
+			jQuery(document).ready(function(){
+				sendRequestByAjax(null);
+				jQuery('#menu li a').click(function(){
+						//alert(this.getAttribute('nav_url'));
+						//ajax
+						sendRequestByAjax(this.getAttribute('nav_url'));
+					}
+				);
+				}
+			);
+			
+			function sendRequestByAjax(nav_url){
+				if(null == nav_url){
+					nav_url = 'consumTravelService';
+				}
+				var newSrc = 'travelResourceManage/' + nav_url + '.php';
+				if (window.XMLHttpRequest)
+				{
+					// IE7+, Firefox, Chrome, Opera, Safari 浏览器执行的代码
+					xmlhttp=new XMLHttpRequest();
+				}
+				else
+				{	
+					//IE6, IE5 浏览器执行的代码
+					xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+				}
+				xmlhttp.onreadystatechange=function()
+				{
+					if (xmlhttp.readyState==4 && xmlhttp.status==200)
+					{
+						
+						//document.getElementById("menuContentOfIframe").src=newSrc;
+						
+						document.getElementById('menuContent').innerHTML=xmlhttp.responseText;
+						
+					}
+				}
+				xmlhttp.open("GET",newSrc,true);
+				xmlhttp.send();
+				
+			}
+		</script>
 	</head>
 <body>
-	<link rel="stylesheet" href="css/resourcesManage.css" type="text/css" />
-	<link rel="stylesheet" href="css/resourceManageTopPic.css" type="text/css" />
-	<link rel="stylesheet" href="css/main.css" type="text/css" />
-	<link rel="stylesheet" href="css/secondMenu.css" type="text/css" />
-	<script src="js/jquery-1.7.1.min.js" type="text/javascript"></script>
-	<script src="js/secondMenu.js" type="text/javascript"></script>
+	
 	<?php include 'menu.php';?>
 	<div class="resourceManageTopPic">	
 		<!--
@@ -21,17 +66,19 @@
 	<!--sencod menu -->
 	<div id="menu">
 	  <ul>
-		<li><a href="#">旅游制定服务</a></li>
-		<li><a href="#">旅游目的管理</a></li>
-		<li><a href="#">旅游APP研发</a></li>
-		<li><a href="#">影视APD海外公关</a></li>
+		<li><a href="javascript:void(0);" nav_url='consumTravelService'>旅游制定服务</a></li>
+		<li><a href="javascript:void(0);" nav_url='manageTravelDestination'>旅游目的管理</a></li>
+		<li><a href="javascript:void(0);" nav_url='travelAppDevelop'>旅游APP研发</a></li>
+		<li><a href="javascript:void(0);" nav_url='filmMake'>影视APD海外公关</a></li>
 	  </ul>
 	  <!--
 		<div class="cls"></div>
 	  -->
 	</div>
-	<div id="menuContent">                                                         				订制旅行，我们以人为本，从心出发，做有责任的业者。专注打造差异化、主题明确的原创旅行产品，深度体验目的地自然风光与人文历史。从非洲出发，我们一路精彩：穿越非洲大陆56天全行程；中国定制“非洲”邮轮首航；自驾南非五国首发；创建《非洲生态营课程》体系；深入斯威士兰，亲历芦苇节“选妃”；第一次以时尚的态度打量非洲-“纳”不一样；进取南美，打造“南美玩家”极地跨越，泛美公路自驾，将是下一个里程碑。开启全球订制模式，打造不一样的旅行体验。
+	<div id="menuContent">
+		
 	</div>
+	
 	<?php include 'foot.php'?>
 
 </body>
---
+++ showSecondMenuContent.php
@@ -0,0 +1,8 @@
+<?php
+/*
+ *通过ajax请求显示二级菜单内容
+ */
+$secondMenuUrl = $_GET['nav_url'];
+
+ echo $secondMenuUrl;
+?>
