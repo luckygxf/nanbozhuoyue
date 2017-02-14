8ffe517126330459119797a3e7d5372fb9fe7825
luckygxf <403261934@qq.com>
2017-02-15 01:02:14 +0800

    index page top pic

--- index.php
+++ index.php
@@ -19,40 +19,46 @@
 	
 	<?php include 'menu.php';?>
 	
-	<div class="topPicture">	
-		<div class="text" id="mainPageText">
-			旅游资源管理与定制
+	<!--
+		<div class="topPicture">	
+			<div class="text" id="mainPageText">
+				旅游资源管理与定制
+			</div>
+		</div>	
+	-->
+	<!--图片轮播实现-->
+	<!--
+		<div class="Container">
+			<div class="image" id="bigpics" title=""><img src="temp/four_find/s1f.jpg" /></div>
+			<div class="switch">
+				<div class="icon1"><a href="#" title="上一个" onfocus="this.blur();">上一个</a></div>
+				<div class="switch_center" id="pics">
+					<ul>
+						<li><a href="#" class="on"><img src="temp/four_find/s1.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
+						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
+					</ul>
+			  </div>
+				<div class="icon2"><a href="#" title="下一个" onfocus="this.blur();">下一个</a></div>
+				<div class="clear"></div>
+			</div>		
 		</div>
-	</div>	
-	<div class="Container">
-		<div class="image" id="bigpics" title=""><img src="temp/four_find/s1f.jpg" /></div>
-		<div class="switch">
-			<div class="icon1"><a href="#" title="上一个" onfocus="this.blur();">上一个</a></div>
-			<div class="switch_center" id="pics">
-				<ul>
-					<li><a href="#" class="on"><img src="temp/four_find/s1.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-					<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-				</ul>
-		  </div>
-			<div class="icon2"><a href="#" title="下一个" onfocus="this.blur();">下一个</a></div>
-			<div class="clear"></div>
-		</div>		
-	</div>	
+	-->	
+	<?php include 'lunbo.php'?>
 	<?php include 'foot.php'?>
 	
 	<!--<script type="text/javascript" src="mainPageAutoMove.js"></script>-->	
---
+++ lunbo.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>APICS</title><script type="text/javascript" src="js/jquery.min.js"></script>
+<script type="text/javascript" src="js/lunbo.js"></script>
+<link href="css/lunbo.css" type="text/css" rel="stylesheet" /></head>
+<body>
+
+<div class="slider-container">
+	<div class="main-slider">  
+	        
+		<div class="slide-item" style="display:block;">
+			<img width="100%" height="350px" src="images/1.jpg" /> 
+			<div class="slide-cont"> 
+				<h1>资源管理vs旅游定制</h1> 			
+			</div> 
+		</div>
+	
+		<div class="slide-item">
+			<img width="100%" height="350px" src="images/2.jpg" />
+			<div class="slide-cont"> 
+				<h1>国际商务</h1>
+			</div> 
+		</div>
+		<div class="slide-item">
+			<img width="100%" height="350px" src="images/3.jpg"/>
+			<div class="slide-cont">
+				<h1>媒体传播</h1>
+			</div>
+		</div>
+	</div>
+	<ul class="thumbs clearfix">
+		<li class="thumb1 curr"><div></div></li>
+		<li class="thumb2"><div></div></li>
+		<li class="thumb3"><div></div></li>
+	</ul>
+</div>
+</body>
+</html>
