81688d2be618e7c20049004fe146d18a6ae96e0b
luckygxf <403261934@qq.com>
2017-02-15 19:02:33 +0800

    clean main page

--- index.php
+++ index.php
@@ -5,10 +5,7 @@
 		
 	</head>
 <body>
-	<!--
-		<link rel="stylesheet" href="firstMenu.css" type="text/css" />
-		<link rel="stylesheet" href="css/topPicture.css" type="text/css" />
-	-->
+
 	<link rel="stylesheet" href="css/main.css" type="text/css" />
 	<link href="css/pics.css" rel="stylesheet" type="text/css" />
 	<link rel="stylesheet" href="css/topPicture.css" type="text/css" />
@@ -18,50 +15,10 @@
 	<script type="text/javascript" src="js/images.js"></script>
 	
 	<?php include 'menu.php';?>
-	
-	<!--
-		<div class="topPicture">	
-			<div class="text" id="mainPageText">
-				旅游资源管理与定制
-			</div>
-		</div>	
-	-->
-	<!--图片轮播实现-->
-	<!--
-		<div class="Container">
-			<div class="image" id="bigpics" title=""><img src="temp/four_find/s1f.jpg" /></div>
-			<div class="switch">
-				<div class="icon1"><a href="#" title="上一个" onfocus="this.blur();">上一个</a></div>
-				<div class="switch_center" id="pics">
-					<ul>
-						<li><a href="#" class="on"><img src="temp/four_find/s1.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s2.png" /></a></li>
-						<li><a href="#"><img src="temp/four_find/s1.png" /></a></li>
-					</ul>
-			  </div>
-				<div class="icon2"><a href="#" title="下一个" onfocus="this.blur();">下一个</a></div>
-				<div class="clear"></div>
-			</div>		
-		</div>
-	-->	
+
 	<?php include 'lunbo.php'?>
 	<?php include 'foot.php'?>
 	
-	<!--<script type="text/javascript" src="mainPageAutoMove.js"></script>-->	
 </body>
 
 </html>
