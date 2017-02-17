5adfdd47e59c9c5d56010b216361c6bf1e4aa286
luckygxf <403261934@qq.com>
2017-02-18 01:07:12 +0800

    clean code

--- resourcesManage_test.php
+++ resourcesManage_test.php
@@ -8,13 +8,11 @@
 <!-- Begin
 if (screen.width <= 1440)
 {
-document.write('<link href="images/qcss1440.css" rel="stylesheet" type="text/css" />');
-
+	document.write('<link href="images/qcss1440.css" rel="stylesheet" type="text/css" />');
 } 
 else
 {
-document.write('<link href="images/qcss.css" rel="stylesheet" type="text/css" />');
-
+	document.write('<link href="images/qcss.css" rel="stylesheet" type="text/css" />');
 } 
 
 </SCRIPT>
@@ -38,9 +36,9 @@ document.write('<link href="images/qcss.css" rel="stylesheet" type="text/css" />
 <script type="text/javascript" src="js/baigie.job.social.js"></script>
 
 <script type="text/javascript">
-    $(document).ready(function(){
-        console.log("in script");
-    });
+$(document).ready(function(){
+	console.log("in script");
+});
 </script>
 
 </head>
@@ -48,184 +46,154 @@ document.write('<link href="images/qcss.css" rel="stylesheet" type="text/css" />
 <body>
 
 <div class="rootContainer">
-  <div class="wrapper">
-    <header id="pageHeader">
-	
-	<div class="gnav">
-        <div class="gnavInner">
-          <div class="hLogo"><img src="images/logo1.jpg"  height="33" /></div>          
-			<div>
-				<?php include 'menu.php';?>
+	<div class="wrapper">
+		<header id="pageHeader">
+			<div class="gnav">
+				<div class="gnavInner">
+					<div class="hLogo"><img src="images/logo1.jpg"  height="33" /></div>          
+					<div>
+						<?php include 'menu.php';?>
+					</div>
+				</div>
 			</div>
-        </div>
-      </div>
-      <div class="mainVisualCopy scrollChaser"> 
-        <!--<div class="mainVisual mainvMessage">
-      <div class="mainVisualInner"><span>MESSAGE</span><br />
-        応募者へのメッセージ</div>--> 
-      </div>
-    </header>
-    <nav class="lnav">
-      <div class="lnavInner">
-        <div class="pagePrev"><a href="#">前</a></div>
-        <ul>
-          <li><span><a href="javascript:void(0)" tid="index">订制旅行服务</a></span></li>
-          <li><span><a href="javascript:void(0)" tid="workers">旅行产品APP研发</a></span></li>
-          <li><span><a href="javascript:void(0)" tid="voice">旅游目的地管理</a></span></li>
-          <li><span><a href="javascript:void(0)" tid="tips">影视制作海外OPD公关</a></span></li>
-        </ul>
-        <div class="pageNext"><a href="#">次</a></div>
-        <div class="currentLine"></div>
-      </div>
-    </nav>
-    <div class="lnavBg"></div>
-    <div class="wrapper2">
-      <main class="mainContents">
-        <section class="mainBlock"> </section>
-      </main>
-      
-      <!-- Start all inner contents. modify k.kubonaka -->
-	  <?php
-		
-	  ?>
-      
-      <div class="cloneContainer"> 
-        <!-- 旅游订制服务 -->
-        <div class="subInner index">
-          
-          <section class="innerBlock">
-            <p>旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字</p>
-               </section>
-          
-          <section class="innerBlock">
-          <h3 class="dzbt">旅游订制项目标题</h3><span class="dzmo"><a href="#" class="showa" id="a_showmore">更多详细内容</a></span>
-          <div class="clear"></div>
-          <div id="box">
-				<p>旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服</p>
-		  </div>
-         
-     
-          </section>
-          
-
-          
-          <section class="innerBlock">
-         <p>
-         <div class="yema">
-         <a href="#">首页</a> <a href="#">上一页</a> <a href="#">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">下一页</a> <a href="#">尾页</a>
-         </div>
-         <div class="yema2" style=" display:none">
-         <a href="#">1</a> <a href="#">2</a> <a href="#">3</a> <a href="#">4</a>....
-         go<input name="" type="text"  class="tiaozhuan"/>页
-         
-         </div>
-         </p>
-         
-     
-          </section>
-        
-         
-        </div>
-        
-        <!-- 旅游目的地管理 -->
-        <div class="subInner workers">
-        
-        <section class="innerBlock">
-            
-            <p>
-            作为旅游目的地营销公司，南博卓越在海外拥有多年目的地管理经验，并深谙中国市场的发展规律
-与环境，拥有与中国旅游分销商、旅游媒体、综合服务商等良好的合作经验，可根据目的地对中国
-旅游市场发展的需求，制定发展方案及实施计划，帮助目的地迅速拓展市场份额。
-            </p>
-          </section>
-          
-          
-          
-          
-          
-          
-        </div>
-               
-        <!-- 旅游产品APP研发 -->
-        <div class="subInner voice">
-          <header class="mainBlockHeader">
-        
-            <p>旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理</p>
-            
-          </header>
-          
-          
-          
-          
-          
-          
-        </div>
-        
-        <!-- 影视海外OPD公关 -->
-        <div class="subInner tips">
-          <header class="mainBlockHeader">
-          
-            <p>影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关</p>
-             <p>影视海外OPD公关影视海外OPD公关</p>
-            
-          </header>
-        
-          
-        </div>
-      </div>
-      
-    
-      <article class="boxArticleEntry">
-        
-      </article>
-      
-    </div>
-    
-  </div>
+			<div class="mainVisualCopy scrollChaser"> 
+				<!--<div class="mainVisual mainvMessage">
+				<div class="mainVisualInner"><span>MESSAGE</span><br />
+				応募者へのメッセージ</div>--> 
+			</div>
+		</header>
+	<nav class="lnav">
+		<div class="lnavInner">
+			<div class="pagePrev"><a href="#">前</a></div>
+				<ul>
+				  <li><span><a href="javascript:void(0)" tid="index">订制旅行服务</a></span></li>
+				  <li><span><a href="javascript:void(0)" tid="workers">旅行产品APP研发</a></span></li>
+				  <li><span><a href="javascript:void(0)" tid="voice">旅游目的地管理</a></span></li>
+				  <li><span><a href="javascript:void(0)" tid="tips">影视制作海外OPD公关</a></span></li>
+				</ul>
+			<div class="pageNext"><a href="#">次</a></div>
+			<div class="currentLine"></div>
+		</div>
+	</nav>
+	<div class="lnavBg"></div>
+	<div class="wrapper2">
+		<main class="mainContents">
+		<section class="mainBlock"> </section>
+		</main>
   
-  <div class="mainVisualCopyInner"><span class="mainText">资源管理VS旅行定制</span> <span class="subText"></span> </div>
-  <div class="scrollVisualWrapper">
-    <div class="mainVisual mainvVision"></div>
-  </div>
+  <!-- Start all inner contents. modify k.kubonaka -->
   
-
-
+		<div class="cloneContainer"> 
+			<!-- 旅游订制服务 -->
+			<div class="subInner index">	  
+				<section class="innerBlock">
+					<p>旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字
+					</p>
+				</section>	  
+				<section class="innerBlock">
+					<h3 class="dzbt">旅游订制项目标题</h3>
+					<span class="dzmo"><a href="#" class="showa" id="a_showmore">更多详细内容</a></span>
+					<div class="clear"></div>
+					<div id="box">
+						<p>旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服务说明文字旅游订制服
+						</p>
+					</div> 
+				</section>
+				<section class="innerBlock">
+					<p>
+						<div class="yema">
+							<a href="#">首页</a> 
+							<a href="#">上一页</a> 
+							<a href="#">1</a> 
+							<a href="#">2</a> 
+							<a href="#">3</a> 
+							<a href="#">下一页</a> 
+							<a href="#">尾页</a>
+						</div>
+						<div class="yema2" style=" display:none">
+							<a href="#">1</a> 
+							<a href="#">2</a> 
+							<a href="#">3</a> 
+							<a href="#">4</a>....go
+							<input name="" type="text"  class="tiaozhuan"/>页	 
+						</div>
+					</p> 
+				</section>	 
+			</div>	
+			<!-- 旅游目的地管理 -->
+			<div class="subInner workers">	
+				<section class="innerBlock">
+					<p>作为旅游目的地营销公司，南博卓越在海外拥有多年目的地管理经验，并深谙中国市场的发展规律与环境，拥有与中国旅游分销商、旅游媒体、综合服务商等良好的合作经验，可根据目的地对中国旅游市场发展的需求，制定发展方案及实施计划，帮助目的地迅速拓展市场份额。
+					</p>
+				</section>	  
+			</div>		   
+			<!-- 旅游产品APP研发 -->
+			<div class="subInner voice">
+				<header class="mainBlockHeader">
+					<p>旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理旅游目的地管理
+					</p>		
+				</header>	  
+			</div>	
+			<!-- 影视海外OPD公关 -->
+			<div class="subInner tips">
+				<header class="mainBlockHeader">
+					<p>影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关影视海外OPD公关
+					</p>
+					<p>
+						影视海外OPD公关影视海外OPD公关
+					</p>		
+				</header>	  
+			</div>
+		</div>
+		<article class="boxArticleEntry"></article>  
+	</div>
+	</div>
+	<div class="mainVisualCopyInner">
+		<span class="mainText">资源管理VS旅行定制</span>
+		<span class="subText"></span>
+	</div>
+	<div class="scrollVisualWrapper">
+		<div class="mainVisual mainvVision"></div>
+	</div>
 </div>
+<div id="goToPageTop">
+	<a href="#pageHeader">返回顶部</a>
 </div>
-<div id="goToPageTop"><a href="#pageHeader">返回顶部</a></div>
+
 <div class="foot">
-<div class="foot1">
-<div class="lxfs">
-<h3 style="color:#FFFFFF">010-57208195
-</h3>
-<p>
-<span style="line-height:30px;color:#FFFFFF">
-邮箱：nbzy@gmmg.com<br />
-QQ：123456789</span>
-</p>
-</div>
-<div class="djz">
-<p><a href="#">项目合作</a> | <a href="#">人才招聘</a> | <a href="#">联系我们</a></p>
-<div  class="huoban">
-<em>合作伙伴</em>
-<ul>
-<li><a href="#"><img src="images/d1.png"  height="30" /></a></li>
-<li><a href="#"><img src="images/d2.png"  height="30" /></a></li>
-<li><a href="#"><img src="images/d3.png"  height="30" /></a></li>
-<li><a href="#"><img src="images/d4.png"  height="30" /></a></li>
-<li><a href="#"><img src="images/d5.png"  height="30" /></a></li>
-</ul>
-</div>
-
-
-
-</div>
-<div class="erweima"><p>关注我们</p><img src="images/erweima.jpg" width="110"/></div>
-<div class="clear"></div>
-</div>
-<div class="foot2">
-京ICP备11017824号-4 / 京ICP证130164号  Copyright © 2006-2016 ZCOOL
-</div>
+	<div class="foot1">
+		<div class="lxfs">
+			<h3 style="color:#FFFFFF">010-57208195</h3>
+			<p>
+				<span style="line-height:30px;color:#FFFFFF">邮箱：nbzy@gmmg.com<br />QQ：123456789</span>
+			</p>
+		</div>
+		<div class="djz">
+			<p>
+				<a href="#">项目合作</a> | 
+				<a href="#">人才招聘</a> | 
+				<a href="#">联系我们</a>
+			</p>
+			<div  class="huoban">
+				<em>合作伙伴</em>
+				<ul>
+					<li><a href="#"><img src="images/d1.png"  height="30" /></a></li>
+					<li><a href="#"><img src="images/d2.png"  height="30" /></a></li>
+					<li><a href="#"><img src="images/d3.png"  height="30" /></a></li>
+					<li><a href="#"><img src="images/d4.png"  height="30" /></a></li>
+					<li><a href="#"><img src="images/d5.png"  height="30" /></a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="erweima">
+			<p>关注我们</p>
+			<img src="images/erweima.jpg" width="110"/>
+		</div>
+		<div class="clear"></div>
+	</div>
+	<div class="foot2">
+		京ICP备11017824号-4 / 京ICP证130164号  Copyright © 2006-2016 ZCOOL
+	</div>
 </div>
 </body>
 </html>
