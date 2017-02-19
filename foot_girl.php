35ddaa233b231cd554f05263460b77a389b60331
luckygxf <403261934@qq.com>
2017-02-19 23:58:20 +0800

    replace foot with girl

---
+++ foot_girl.php
@@ -0,0 +1,35 @@
+<link rel="stylesheet" href="css/qcss1440.css" type="text/css" />
+<div class="foot">
+<div class="foot1">
+<div class="lxfs">
+<h3 style="color:#FFFFFF">010-57208195
+</h3>
+<p>
+<span style="line-height:30px;color:#FFFFFF">
+邮箱：nbzy@gmmg.com<br />
+QQ：123456789</span>
+</p>
+</div>
+<div class="djz">
+<p><a href="#">项目合作</a> | <a href="#">人才招聘</a> | <a href="#">联系我们</a></p>
+<div  class="huoban">
+<em>合作伙伴</em>
+<ul>
+<li><a href="#"><img src="images/d1.png"  height="30" /></a></li>
+<li><a href="#"><img src="images/d2.png"  height="30" /></a></li>
+<li><a href="#"><img src="images/d3.png"  height="30" /></a></li>
+<li><a href="#"><img src="images/d4.png"  height="30" /></a></li>
+<li><a href="#"><img src="images/d5.png"  height="30" /></a></li>
+</ul>
+</div>
+
+
+
+</div>
+<div class="erweima"><p>关注我们</p><img src="images/erweima.jpg" width="110"/></div>
+<div class="clear"></div>
+</div>
+<div class="foot2">
+京ICP备11017824号-4 / 京ICP证130164号  Copyright ? 2006-2016 ZCOOL
+</div>
+</div>
